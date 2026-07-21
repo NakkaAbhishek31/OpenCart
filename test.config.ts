56810c5ca0a334cc10e806cd2d60c17bdb54a1f3
NakkaAbhishek31 <abhisheknakka22@gmail.com>
2026-07-21 14:47:00 +0530

    emial and password changed

--- test.config.ts
+++ test.config.ts
@@ -1,10 +1,10 @@
 export class TestConfig{
-    appUrl="http://localhost/opencart/upload"
+    //appUrl="http://localhost/opencart/upload"
     //appUrl="https://naveenautomationlabs.com/opencart"
-    //appUrl="https://tutorialsninja.com/demo/"
+    appUrl="https://tutorialsninja.com/demo/"
     //valid login credentitals --create own login account 
-    email="abhitest@gmail.com"
-password="test@31"
+    email="test-playwright@gmail.com"
+password="test@123"
 
      //product details 
      productName="MacBook";
