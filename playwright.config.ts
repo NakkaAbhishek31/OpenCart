d39fc5922fa2063a45cbb9ba37330e7fbb4c6c2d
NakkaAbhishek31 <abhisheknakka22@gmail.com>
2026-07-21 11:36:14 +0530

    updated playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
  retries:1,
   /* Opt out of parallel tests on CI. */
   //workers: process.env.CI ? 1 : undefined,
-  //workers:1,
+  workers:1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
 reporter: [
 ['html'],
