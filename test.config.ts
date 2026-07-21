9b3c3af4125587919b304b6e80437636491f6ea4
NakkaAbhishek31 <abhisheknakka22@gmail.com>
2026-07-21 11:46:27 +0530

    updated test.config.ts

--- test.config.ts
+++ test.config.ts
@@ -1,5 +1,7 @@
 export class TestConfig{
-    appUrl="http://localhost/opencart/upload"
+    //appUrl="http://localhost/opencart/upload"
+      //appUrl="https://naveenautomationlabs.com/opencart"
+    appUrl="https://tutorialsninja.com/demo/"
     //valid login credentitals --create own login account 
     email="abhitest@gmail.com"
 password="test@31"
