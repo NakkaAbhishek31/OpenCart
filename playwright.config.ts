d27045892fc6594cd57aea4eaa9d4a14eec3f487
NakkaAbhishek31 <abhisheknakka22@gmail.com>
2026-07-21 12:00:33 +0530

    updated fix

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,91 +1,48 @@
 import { defineConfig, devices } from '@playwright/test';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
-
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
-  timeout: 30 * 1000,
+  timeout: 30 * 1000,   //30000 ms(30 secs)
   testDir: './tests',
-  /* Run tests in files in parallel */
   fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
- // retries: process.env.CI ? 2 : 0,
- retries:1,
-  /* Opt out of parallel tests on CI. */
+  //retries: process.env.CI ? 2 : 0,
+  retries:1,
   //workers: process.env.CI ? 1 : undefined,
-  workers:1,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-reporter: [
-['html'],
-['allure-playwright'],
-['dot'],
-['list']
-],
+  workers: 1,
 
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-use: {
-trace: 'on-first-retry',
-screenshot: 'only-on-failure',
-video: 'retain-on-failure',
-//headless: false,
-viewport: { width: 1280, height: 720 }, // Set default viewport size for consistency
-ignoreHTTPSErrors: true, // Ignore SSL errors if necessary
-permissions: ['geolocation'], // Set necessary permissions for geolocation-based tests
-},
-//grep: /@master/,
+  reporter: [
+    ['html'],
+    ['allure-playwright'],
+    //['dot'],
+    ['list']
+  ],
 
-  /* Configure projects for major browsers */
+  use: {
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+    //headless: false,
+    viewport: { width: 1280, height: 720 }, // Set default viewport size for consistency
+    ignoreHTTPSErrors: true, // Ignore SSL errors if necessary
+    permissions: ['geolocation'], // Set necessary permissions for geolocation-based tests
+  },
+
+  //grep: /@master/,
+
   projects: [
-    {
+   {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
+    /*{
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
 
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
-
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    } */
   ],
 
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+
 });
